89a422c35d8dbdb0a19215dcf698d7a8efafc27c
luuducquang <luuquang203@gmail.com>
2024-05-01 22:53:24 +0700

    update

--- FRONT_END_MYPHAM/front-end-mypham-admin/src/pages/Product/Product.tsx
+++ FRONT_END_MYPHAM/front-end-mypham-admin/src/pages/Product/Product.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { searchProduct } from "../../service/product.service";
 import { useParams } from "react-router-dom";
-import { Table, TableColumnsType, Pagination } from "antd";
+import { Table, TableColumnsType, Pagination, notification } from "antd";
 import { apiImage } from "../../constant/api";
 import { FaInfoCircle, FaStar } from "react-icons/fa";
 import ProductModal from "../../components/HandlerProduct/ProductModal";
@@ -23,6 +23,8 @@ interface DataType {
     trangThai: any;
 }
 
+type NotificationType = "success" | "info" | "warning" | "error";
+
 function Product() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -42,6 +44,22 @@ function Product() {
 
     const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
 
+    const [keySearch, setKeySearch] = useState("TenSanPham");
+
+    const [valueSearch, setValueSearch] = useState("");
+
+    const [api, contextHolder] = notification.useNotification();
+
+    const openNotificationWithIcon = (
+        type: NotificationType,
+        content: string
+    ) => {
+        api[type]({
+            message: "Thông báo",
+            description: content,
+        });
+    };
+
     const handleClearSelection = () => {
         setSelectedRowKeys([]);
     };
@@ -58,11 +76,12 @@ function Product() {
         setIsOpenDeleteModal(false);
     };
 
-    const fetchData = async () => {
+    const fetchData = async (keySearch: string, valueSearch: any) => {
         setLoading(true);
         let results = await searchProduct({
             page: currentPage,
             pageSize: 10,
+            [keySearch]: valueSearch,
         });
         setData(results.data);
         setTotalProducts(results.totalItems);
@@ -70,8 +89,17 @@ function Product() {
     };
 
     useEffect(() => {
-        fetchData();
+        fetchData(keySearch, valueSearch);
     }, [currentPage]);
+
+    const handleSearch = (event: any) => {
+        event.preventDefault();
+        if (keySearch != "") {
+            fetchData(keySearch, valueSearch);
+        } else {
+            openNotificationWithIcon("warning", "Vui lòng chọn loại tìm kiếm!");
+        }
+    };
 
     const dataSet = data.map(function (value: any, index: any) {
         return {
@@ -196,144 +224,167 @@ function Product() {
         }),
     };
     return (
-        <div className="container">
-            <form className="form-group">
-                <div className="row g-2 align-items-center mb-3">
-                    <div className="col">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Nhập từ khoá cần tìm"
-                        />
+        <>
+            {contextHolder}
+            <div className="container">
+                <form className="form-group" onSubmit={handleSearch}>
+                    <div className="row g-2 align-items-center mb-3">
+                        <div className="col">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Nhập từ khoá cần tìm"
+                                value={valueSearch}
+                                onChange={(e) => setValueSearch(e.target.value)}
+                            />
+                        </div>
+                        <div className="col-auto">
+                            <select
+                                className="form-select"
+                                aria-label="Default select example"
+                                value={keySearch}
+                                onChange={(e) => setKeySearch(e.target.value)}
+                            >
+                                <option value="" disabled>
+                                    Tìm kiếm theo
+                                </option>
+                                <option value="TenSanPham">Tên Sản Phẩm</option>
+                                <option value="TenDanhMuc">Tên Danh Mục</option>
+                                <option value="Tendanhmucuudai">
+                                    Tên Danh Mục Ưu Đãi
+                                </option>
+                                <option value="TenNhaPhanPhoi">
+                                    Tên Nhà Phân Phối
+                                </option>
+                                <option value="TenHang">
+                                    Tên Hãng Sản Xuất
+                                </option>
+                                <option value="XuatXu">Xuất Xứ</option>
+                            </select>
+                        </div>
+                        <div className="col-auto">
+                            <button
+                                className="btn btn-primary d-flex align-items-center"
+                                type="button"
+                                onClick={handleSearch}
+                            >
+                                <IoSearch className="mr-1" />
+                                Tìm kiếm
+                            </button>
+                        </div>
                     </div>
-                    <div className="col-auto">
-                        <select
-                            className="form-select"
-                            aria-label="Default select example"
-                        >
-                            <option value="">Tìm kiếm theo</option>
-                            <option value="TenSanPham">Tên Sản Phẩm</option>
-                            <option value="TenDanhMuc">Tên Danh Mục</option>
-                            <option value="Tendanhmucuudai">
-                                Tên Danh Mục Ưu Đãi
-                            </option>
-                            <option value="TenNhaPhanPhoi">
-                                Tên Nhà Phân Phối
-                            </option>
-                            <option value="TenHang">Tên Hãng Sản Xuất</option>
-                            <option value="XuatXu">Xuất Xứ</option>
-                        </select>
+                    <div
+                        className="row"
+                        style={{ width: "50%", marginLeft: 3 }}
+                    >
+                        <div className="range-container col">
+                            <label className="range-label" htmlFor="giaMin">
+                                Giá Min:
+                            </label>
+                            <input
+                                type="range"
+                                className="form-range"
+                                id="giaMin"
+                                min={0}
+                                max={4999999}
+                                step={1}
+                                defaultValue={0}
+                            />
+                            <span className="range-value" id="giaMinValue">
+                                0
+                            </span>
+                        </div>
+                        <div className="range-container col">
+                            <label className="range-label" htmlFor="giaMax">
+                                Giá Max:
+                            </label>
+                            <input
+                                type="range"
+                                className="form-range"
+                                id="giaMax"
+                                min={0}
+                                max={4999999}
+                                step={1}
+                                defaultValue={4999999}
+                            />
+                            <span className="range-value" id="giaMaxValue">
+                                4999999
+                            </span>
+                        </div>
                     </div>
-                    <div className="col-auto">
-                        <button
-                            className="btn btn-primary d-flex align-items-center"
-                            type="button"
-                        >
-                            <IoSearch className="mr-1" /> Tìm kiếm
-                        </button>
-                    </div>
-                </div>
-                <div className="row" style={{ width: "50%", marginLeft: 3 }}>
-                    <div className="range-container col">
-                        <label className="range-label" htmlFor="giaMin">
-                            Giá Min:
-                        </label>
-                        <input
-                            type="range"
-                            className="form-range"
-                            id="giaMin"
-                            min={0}
-                            max={4999999}
-                            step={1}
-                            defaultValue={0}
-                        />
-                        <span className="range-value" id="giaMinValue">
-                            0
-                        </span>
-                    </div>
-                    <div className="range-container col">
-                        <label className="range-label" htmlFor="giaMax">
-                            Giá Max:
-                        </label>
-                        <input
-                            type="range"
-                            className="form-range"
-                            id="giaMax"
-                            min={0}
-                            max={4999999}
-                            step={1}
-                            defaultValue={4999999}
-                        />
-                        <span className="range-value" id="giaMaxValue">
-                            4999999
-                        </span>
-                    </div>
-                </div>
-            </form>
+                </form>
 
-            <div className="button mb-3">
-                <button
-                    onClick={() => {
-                        showModal();
-                        setMaSanPham(undefined);
+                <div className="button mb-3">
+                    <button
+                        onClick={() => {
+                            showModal();
+                            setMaSanPham(undefined);
+                        }}
+                        type="button"
+                        className="btn btn-success btn-add"
+                    >
+                        <i className="fa-solid fa-plus" /> Thêm sản phẩm
+                    </button>
+                    <button
+                        onClick={() => {
+                            if (listIdDelete.length > 0) {
+                                setIsOpenDeleteModal(true);
+                            }
+                        }}
+                        type="button"
+                        className="btn btn-danger btn-del mx-1"
+                    >
+                        <i className="fa-solid fa-trash" /> Xoá sản phẩm
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-primary btn-up5 mx-1"
+                    >
+                        <i className="fa-solid fa-circle-up" /> Tăng 5% giá bán
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-secondary btn-down5"
+                    >
+                        <i className="fa-solid fa-circle-down" /> Giảm 5% giá
+                        bán
+                    </button>
+                </div>
+                <Table
+                    bordered={true}
+                    rowSelection={{
+                        ...rowSelection,
+                        selectedRowKeys: selectedRowKeys,
                     }}
-                    type="button"
-                    className="btn btn-success btn-add"
-                >
-                    <i className="fa-solid fa-plus" /> Thêm sản phẩm
-                </button>
-                <button
-                    onClick={() => {
-                        if (listIdDelete.length > 0) {
-                            setIsOpenDeleteModal(true);
-                        }
-                    }}
-                    type="button"
-                    className="btn btn-danger btn-del mx-1"
-                >
-                    <i className="fa-solid fa-trash" /> Xoá sản phẩm
-                </button>
-                <button type="button" className="btn btn-primary btn-up5 mx-1">
-                    <i className="fa-solid fa-circle-up" /> Tăng 5% giá bán
-                </button>
-                <button type="button" className="btn btn-secondary btn-down5">
-                    <i className="fa-solid fa-circle-down" /> Giảm 5% giá bán
-                </button>
+                    columns={columns}
+                    dataSource={dataSet}
+                    loading={loading}
+                    rowClassName="hover-row"
+                    pagination={false}
+                />
+                <Pagination
+                    current={currentPage}
+                    total={totalProducts}
+                    pageSize={10}
+                    onChange={handlePageChange}
+                    style={{ marginTop: "16px", textAlign: "center" }}
+                />
+                <ProductModal
+                    showModal={showModal}
+                    isModalOpen={isModalOpen}
+                    handleCancelIUModal={handleCancelIUModal}
+                    refreshData={fetchData}
+                    maSanPham={maSanPham}
+                />
+                <ProductDelete
+                    isOpenDeleteModal={isOpenDeleteModal}
+                    fetchData={fetchData}
+                    handleCancelDeleteModal={handleCancelDeleteModal}
+                    listiddel={listIdDelete}
+                    onDeleteSuccess={handleClearSelection}
+                />
             </div>
-            <Table
-                bordered={true}
-                rowSelection={{
-                    ...rowSelection,
-                    selectedRowKeys: selectedRowKeys,
-                }}
-                columns={columns}
-                dataSource={dataSet}
-                loading={loading}
-                rowClassName="hover-row"
-                pagination={false}
-            />
-            <Pagination
-                current={currentPage}
-                total={totalProducts}
-                pageSize={10}
-                onChange={handlePageChange}
-                style={{ marginTop: "16px", textAlign: "center" }}
-            />
-            <ProductModal
-                showModal={showModal}
-                isModalOpen={isModalOpen}
-                handleCancelIUModal={handleCancelIUModal}
-                refreshData={fetchData}
-                maSanPham={maSanPham}
-            />
-            <ProductDelete
-                isOpenDeleteModal={isOpenDeleteModal}
-                fetchData={fetchData}
-                handleCancelDeleteModal={handleCancelDeleteModal}
-                listiddel={listIdDelete}
-                onDeleteSuccess={handleClearSelection}
-            />
-        </div>
+        </>
     );
 }
 
